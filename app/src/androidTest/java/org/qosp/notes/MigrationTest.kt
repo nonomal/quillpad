42bb6036934ebf859ccf0299464718554e2186e3
Arumugam J <sixface@msn.com>
2024-07-09 13:28:49 -0400

    DB migration - WIP

--- app/src/androidTest/java/org/qosp/notes/MigrationTest.kt
+++ app/src/androidTest/java/org/qosp/notes/MigrationTest.kt
@@ -5,11 +5,11 @@ import androidx.room.testing.MigrationTestHelper
 import androidx.sqlite.db.framework.FrameworkSQLiteOpenHelperFactory
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
-import java.io.IOException
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.qosp.notes.data.AppDatabase
+import java.io.IOException
 
 @RunWith(AndroidJUnit4::class)
 class MigrationTest {
@@ -49,6 +49,41 @@ class MigrationTest {
 
     @Test
     @Throws(IOException::class)
+    fun migrate1To2() {
+        var db = helper.createDatabase(testDb, 2).apply {
+            // db has schema version 1. insert some data using SQL queries.
+            // You cannot use DAO classes because they expect the latest schema.
+            execSQL(
+                """
+                insert into cloud_ids(mappingId, localNoteId, remoteNoteId, provider, isDeletedLocally, isBeingUpdated)
+                values ( 1, 33, 44, "nextcloud", 0, 0 );
+            """.trimIndent()
+            )
+
+            //     val mappingId: Long = 0L,
+            //    val localNoteId: Long,
+            //    val remoteNoteId: Long?,
+            //    val provider: CloudService?,
+            //    val extras: String?,
+            //    val isDeletedLocally: Boolean,
+            //    val isBeingUpdated: Boolean = false,
+            // Prepare for the next version.
+            close()
+        }
+
+
+        // Re-open the database with version 2 and provide
+        // MIGRATION_1_2 as the migration process.
+//        db = helper.runMigrationsAndValidate(TEST_DB, 2, true, MIGRATION_1_2)
+
+        // MigrationTestHelper automatically verifies the schema changes,
+        // but you need to validate that the data was migrated properly.
+
+    }
+
+
+    @Test
+    @Throws(IOException::class)
     fun migrate2to3() {
         // Create the earliest version of the database.
         helper.createDatabase(testDb, 2).apply {
@@ -64,5 +99,29 @@ class MigrationTest {
         ).addMigrations(*ALL_MIGRATIONS).build().apply {
             openHelper.writableDatabase.close()
         }
+    }
+
+
+    @Test
+    @Throws(IOException::class)
+    fun migrate3to4() {
+        // Create the earliest version of the database.
+        helper.createDatabase(testDb, 3).apply {
+            close()
+        }
+
+        var db = helper.runMigrationsAndValidate(testDb, 4, true, )
+
+
+
+        // Open latest version of the database. Room validates the schema
+        // once all migrations execute.
+//        Room.databaseBuilder(
+//            InstrumentationRegistry.getInstrumentation().targetContext,
+//            AppDatabase::class.java,
+//            testDb
+//        ).addMigrations(*ALL_MIGRATIONS).build().apply {
+//            openHelper.writableDatabase.close()
+//        }
     }
 }
